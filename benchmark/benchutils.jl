340a260a1265f14367bf8be339ec800b7cb28fff
Dillon Daudert <dillongdaudert@gmail.com>
2018-10-14 11:38:23 -0400

    Some benchmark utils

--- benchmark/benchutils.jl
+++ benchmark/benchutils.jl
@@ -1,1 +1,10 @@
 # Utilities for benchmarking NN Descent
+using BenchmarkTools
+using NNDescent: _nn_descent, brute_knn
+
+recall(nn, true_nn) = sum(_recall(nn[i,:], true_nn[i,:]) for i in 1:size(nn,1))/size(nn,1)
+_recall(π, πₜ) = length(intersect(π, πₜ))/length(πₜ)
+
+function do_benchmark()
+    return
+end
