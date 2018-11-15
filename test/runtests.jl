45451929dafcfffa07db5f812b63390f6d80e03c
Dillon Daudert <dillongdaudert@gmail.com>
2018-11-15 11:28:18 -0500

    Import heappush into tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 # tests
 using NNDescent
-using NNDescent: NNTuple, _neighbors, brute_knn, _update_nn!
+using NNDescent: NNTuple, _neighbors, brute_knn, _update_nn!, heappush!
 using DataStructures
 using Test
 
