cd1d6dd403e2f44407039d94b74decb2ed00a5be
Dillon Daudert <dillongdaudert@gmail.com>
2018-11-18 14:55:16 -0500

    Transpose 3nn

--- test/nndescent_tests.jl
+++ test/nndescent_tests.jl
@@ -21,14 +21,14 @@ end
             [1., 1., 0.],
             [1., 1., 1.]]
     _3nn = brute_knn(data, Euclidean(), 3)
-    true_3nn = [2 3 5;
+    true_3nn = transpose([2 3 5;
                 1 4 6;
                 1 4 7;
                 2 3 8;
                 1 6 7;
                 2 5 8;
                 3 5 8;
-                4 6 7]
+                4 6 7])
     @testset "brute knn test" begin
         @test getindex.(_3nn, 1) == true_3nn
     end
