607fe4e00b7db74fa091c3f0c4b8a10aa64e0bfc
Dillon Daudert <dillongdaudert@gmail.com>
2018-11-18 14:52:17 -0500

    Return KxN matrix of (id,dist) tuples from brute_knn

--- src/brute_knn.jl
+++ src/brute_knn.jl
@@ -1,6 +1,7 @@
 """
 Brute-force (𝒪(n²)) kNN search algorithm.
-Returns an NxK array of the k nearest neighbors for each point in `data`.
+Returns an KxN array of tuples (index, distance) of the k nearest neighbors 
+for each point in `data`.
 """
 function brute_knn(data::Vector{V},
                    metric::Metric,
@@ -11,14 +12,15 @@ function brute_knn(data::Vector{V},
 
     @inbounds @fastmath for i = 1:np, j = 1:np
         d = evaluate(metric, data[i], data[j])
-        distances[i, j] = NNTuple(j, d)
+        distances[j, i] = NNTuple(j, d)
     end
 
     # start at 2 as index 1 is the distance to itself (once sorted)
-    knn = sort(distances, dims=2)[1:end,2:k+1]
-    knn_ids = zeros(Int64, size(knn))
+    knn_graph_tuples = sort(distances, dims=2)[1:end,2:k+1]
+    knn_graph = Matrix{Tuple{Int, Float64}}(undef, k, np)
     for i = 1:np, j = 1:k
-        knn_ids[i, j] = knn[i, j].idx
+        knn_graph[j, i] = (knn_graph_tuples[i, j].idx,
+                           knn_graph_tuples[i, j].dist)
     end
     return knn_ids
 end
