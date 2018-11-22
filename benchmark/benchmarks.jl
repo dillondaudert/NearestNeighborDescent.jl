34187578a9d9bad1e4d2eee4245a9ae8f87e5329
Dillon Daudert <dillongdaudert@gmail.com>
2018-11-22 13:31:24 -0500

    Add brute knn group

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -7,10 +7,11 @@ include("benchutils.jl")
 
 const SUITE = BenchmarkGroup()
 
-SUITE["nndescent"] = BenchmarkGroup(["constructor", "search"])
+SUITE["nndescent"] = BenchmarkGroup(["constructor"])
 SUITE["nndescent"]["rand"] = @benchmarkable DescentGraph($rand_data, 10)
 SUITE["nndescent"]["fmnist"] = @benchmarkable DescentGraph($FMNIST_data, 10)
 SUITE["nndescent"]["mnist"] = @benchmarkable DescentGraph($MNIST_data, 10)
+SUITE["brute"] = BenchmarkGroup(["constructor"])
 SUITE["brute"]["rand"] = @benchmarkable DescentGraph($rand_data, 10)
 SUITE["brute"]["fmnist"] = @benchmarkable brute_knn($FMNIST_data, 10)
 SUITE["brute"]["mnist"] = @benchmarkable brute_knn($MNIST_data, 10)
