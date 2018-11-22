e217272155e0cb960c391cf262b446bb463a8d5d
Dillon Daudert <dillongdaudert@gmail.com>
2018-11-22 13:34:32 -0500

    Use distances in benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,6 +1,7 @@
 using NNDescent
 using NNDescent: brute_knn, brute_search, NNTuple
 using BenchmarkTools
+using Distances
 using PkgBenchmark
 
 include("benchutils.jl")
@@ -12,6 +13,6 @@ SUITE["nndescent"]["rand"] = @benchmarkable DescentGraph($rand_data, 10)
 SUITE["nndescent"]["fmnist"] = @benchmarkable DescentGraph($FMNIST_data, 10)
 SUITE["nndescent"]["mnist"] = @benchmarkable DescentGraph($MNIST_data, 10)
 SUITE["brute"] = BenchmarkGroup(["constructor"])
-SUITE["brute"]["rand"] = @benchmarkable DescentGraph($rand_data, 10)
-SUITE["brute"]["fmnist"] = @benchmarkable brute_knn($FMNIST_data, 10)
-SUITE["brute"]["mnist"] = @benchmarkable brute_knn($MNIST_data, 10)
+SUITE["brute"]["rand"] = @benchmarkable brute_knn($rand_data, Euclidean(), 10)
+SUITE["brute"]["fmnist"] = @benchmarkable brute_knn($FMNIST_data, Euclidean(), 10)
+SUITE["brute"]["mnist"] = @benchmarkable brute_knn($MNIST_data, Euclidean(), 10)
