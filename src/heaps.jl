c6db31243289c5590968f2018c1e0639033cb868
Dillon Daudert <dillongdaudert@gmail.com>
2018-12-08 11:59:47 -0500

    Create AbstractMinMaxHeap type and reimplement heaps interface

--- src/heaps.jl
+++ src/heaps.jl
@@ -1,16 +1,46 @@
-# implementation of arrays as min-max heaps
+using Base.Order: lt, Ordering, Forward, Reverse, ForwardOrdering, ReverseOrdering
 
-using Base.Order: lt, Ordering, Forward, Reverse
+################################################
+#
+# minmax heap type and constructors
+#
+################################################
 
-@inline mm_klargest!(A::AbstractVector, k::Integer) = [heappop_max!(A) for _ in 1:min(length(A), k)]
-@inline mm_ksmallest!(A::AbstractVector, k::Integer) = [heappop_min!(A) for _ in 1:min(length(A), k)]
+abstract type AbstractMinMaxHeap{T} <: AbstractHeap{T} end
 
-function mm_heappush!(A::AbstractVector, x)
-    push!(A, x)
-    bubbleup!(A, length(A))
+mutable struct BinaryMinMaxHeap{T} <: AbstractMinMaxHeap{T}
+    valtree::Vector{T}
+    
+    function BinaryMinMaxHeap{T}() where {T}
+        new{T}(Vector{T}())
+    end
+    
+    function BinaryMinMaxHeap{T}(xs) where {T}
+        valtree = _make_binary_minmax_heap(T, xs)
+        new{T}(valtree)
+    end
 end
 
-function bubbleup!(A::AbstractVector, i::Integer)
+binary_minmax_heap(vt::T) where {T} = BinaryMinMaxHeap{T}()
+binary_minmax_heap(xs::AbstractVector{T}) where {T} = BinaryMinMaxHeap{T}(xs) 
+
+
+################################################
+#
+# core implementation
+#
+################################################
+
+function _make_binary_minmax_heap(vt::Type{T}, xs) where {T}
+    n = length(xs)
+    valtree = copy(xs)
+    for i = n:-1:1
+        _minmax_heap_trickle_down!(valtree, i)
+    end
+    valtree
+end
+
+function _minmax_heap_bubble_up!(A::AbstractVector, i::Integer)
     if level(i) % 2 == 0
         # min level
         if i > 1 && A[i] > A[hparent(i)]
@@ -18,10 +48,10 @@ function bubbleup!(A::AbstractVector, i::Integer)
             tmp = A[i]
             A[i] = A[hparent(i)]
             A[hparent(i)] = tmp
-            bubbleup!(A, hparent(i), Reverse)
+            _minmax_heap_bubble_up!(A, hparent(i), Reverse)
         else
             # bubble up min
-            bubbleup!(A, i, Forward)
+            _minmax_heap_bubble_up!(A, i, Forward)
         end
         
     else
@@ -31,80 +61,41 @@ function bubbleup!(A::AbstractVector, i::Integer)
             tmp = A[i]
             A[i] = A[hparent(i)]
             A[hparent(i)] = tmp
-            bubbleup!(A, hparent(i), Forward)
+            _minmax_heap_bubble_up!(A, hparent(i), Forward)
         else
             # bubble up max
-            bubbleup!(A, i, Reverse)
+            _minmax_heap_bubble_up!(A, i, Reverse)
         end
     end
    return 
 end
 
-function bubbleup!(A::AbstractVector, i::Integer, o::Ordering, x=A[i])
+function _minmax_heap_bubble_up!(A::AbstractVector, i::Integer, o::Ordering, x=A[i])
     gparent = hparent(hparent(i))
     if i > hparent(i) > gparent >= 1
         # i has grandparent
         if lt(o, x, A[gparent])
             A[i] = A[gparent]
             A[gparent] = x
-            bubbleup!(A, gparent, o)
+            _minmax_heap_bubble_up!(A, gparent, o)
         end
     end
     return          
 end
 
-function heappop_min!(A::AbstractVector)
-    x = A[1]
-    y = pop!(A)
-    if !isempty(A)
-        A[1] = y
-        trickledown!(A, 1)
-    end
-    return x
-end
-
-function heappop_max!(A::AbstractVector)
-    x, i = maximum([(A[j], j) for j in 1:min(length(A), 3)])
-    y = pop!(A)
-    if !isempty(A) && i <= length(A)
-        A[i] = y
-        trickledown!(A, i)
-    end
-    return x    
-end
-
-@inline heapmin(A::AbstractVector) = A[1]
-@inline function heapmax(A::AbstractVector)
-    maxlen = min(length(A), 3)
-    els = [A[i] for i in 1:maxlen]
-    return maximum(els)
-end
-
-function minmax_heapify!(A::AbstractVector)
-    for i = length(A):-1:1
-        trickledown!(A, i)
-    end
-    return
-end
-
-"""
-
-"""
-function trickledown!(A::AbstractVector, i::Integer)
+function _minmax_heap_trickle_down!(A::AbstractVector, i::Integer)
     if level(i) % 2 == 0
-        trickledown!(A, i, Forward)
+        _minmax_heap_trickle_down!(A, i, Forward)
     else
-        trickledown!(A, i, Reverse)
+        _minmax_heap_trickle_down!(A, i, Reverse)
     end
     return
 end
 
-function trickledown!(A::AbstractVector, i::Integer, o::Ordering, x=A[i])
+function _minmax_heap_trickle_down!(A::AbstractVector, i::Integer, o::Ordering, x=A[i])
     N = length(A)
     if lchild(i) ≤ N
         # tuple (min(val, j1) max(val, j2))
-        # NOTE: this will find the index of the minimum or maximum descendant,
-        #       depending on which ordering we're interested in
         ext = extrema((A[j], j) for j in descendants(N, i))
         m = lt(o, ext[1], ext[2]) ? ext[1][2] : ext[2][2]
 
@@ -118,7 +109,7 @@ function trickledown!(A::AbstractVector, i::Integer, o::Ordering, x=A[i])
                     A[m] = A[hparent(m)]
                     A[hparent(m)] = t
                 end
-                trickledown!(A, m, o)
+                _minmax_heap_trickle_down!(A, m, o)
             end
         else
             if lt(o, A[m], A[i])
@@ -127,10 +118,15 @@ function trickledown!(A::AbstractVector, i::Integer, o::Ordering, x=A[i])
             end
         end
     end
-    nothing
+    return
 end
-
+                    
+################################################
+#
 # utilities
+#
+################################################
+
 @inline level(i) = round(Int, floor(log2(i)))
 @inline lchild(i) = 2*i
 @inline rchild(i) = 2*i+1
@@ -178,4 +174,92 @@ function is_minmax_heap(A::AbstractVector)
     end
     isheap
 end
+
+################################################
+#
+# interfaces
+#
+################################################
+
+length(h::BinaryMinMaxHeap) = length(h.valtree)
+
+isempty(h::BinaryMinMaxHeap) = isempty(h.valtree)
+
+"""
+    pop!(h::BinaryMinMaxHeap) = popmin!(h)
+"""
+@inline pop!(h::BinaryMinMaxHeap) = popmin!(h)
+
+"""
+    popmin!(h::BinaryMinMaxHeap) -> min
                         
+Remove the minimum value from the heap.
+"""
+function popmin!(h::BinaryMinMaxHeap)
+    valtree = h.valtree
+    x = valtree[1]
+    y = pop!(valtree)
+    if !isempty(valtree)
+        valtree[1] = y
+        _minmax_heap_trickle_down!(valtree, 1)
+    end
+    return x
+end
+
+"""
+    popmax!(h::BinaryMinMaxHeap) -> max
+                        
+Remove the maximum value from the heap.
+"""
+function popmax!(h::BinaryMinMaxHeap)
+    valtree = h.valtree
+    @inbounds x, i = maximum([(valtree[j], j) for j in 1:min(length(valtree), 3)])
+    y = pop!(valtree)
+    if !isempty(valtree) && i <= length(valtree)
+        @inbounds valtree[i] = y
+        _minmax_heap_trickle_down!(valtree, i)
+    end
+    return x    
+end
+
+function push!(h::BinaryMinMaxHeap, v)
+    valtree = h.valtree        
+    push!(valtree, v)
+    _minmax_heap_bubble_up!(valtree, length(valtree))
+end
+
+"""
+    top(h::BinaryMinMaxHeap)
+                        
+Get the top (minimum) of the heap.
+"""
+@inline top(h::BinaryMinMaxHeap) = min(h)
+
+@inline min(h::BinaryMinMaxHeap) = h.valtree[1]
+
+function max(h::BinaryMinMaxHeap) 
+    valtree = h.valtree
+    maxlen = min(length(valtree), 3)
+    @inbounds m = maximum([valtree[i] for i in 1:maxlen])
+    m
+end
+                        
+
+"""
+    empty!(h::BinaryMinMaxHeap, ::Ordering = Forward)
+                        
+Remove and return all the elements of `h` according to
+the given ordering. Default is `Forward` (smallest to 
+largest).
+"""
+empty!(h::BinaryMinMaxHeap) = empty!(h, Forward)
+empty!(h::BinaryMinMaxHeap, ::ForwardOrdering) = ksmallest!(h, length(h))
+empty!(h::BinaryMinMaxHeap, ::ReverseOrdering) = klargest!(h, length(h))
+
+@inline function klargest!(h::BinaryMinMaxHeap, k::Integer) 
+    return [popmax!(h) for _ in 1:min(length(h), k)]                    
+end
+
+@inline function ksmallest!(h::BinaryMinMaxHeap, k::Integer) 
+    return [popmin!(h) for _ in 1:min(length(h), k)]
+end
--- test/heaps_tests.jl
+++ test/heaps_tests.jl
@@ -1,119 +1,125 @@
 # tests for min-max heaps
-using NearestNeighborDescent: is_minmax_heap, minmax_heapify!, heapmin, heapmax, heappop_min!, heappop_max!, mm_heappush!, mm_klargest!, mm_ksmallest!
+using NearestNeighborDescent: binary_minmax_heap, _make_binary_minmax_heap, is_minmax_heap, length, isempty, pop!, popmin!, popmax!, push!, top, min, max, empty!, ksmallest!, klargest!
+using Base.Order: Forward, Reverse
 
-@testset "minmax heaps tests" begin
-
+@testset "Binary MinMax Heaps" begin
+    
     @testset "is_minmax_heap tests" begin
         mmheap = [0, 10, 9, 2, 3, 4, 5]
         @test is_minmax_heap(mmheap)
         @test is_minmax_heap([])
         @test is_minmax_heap([rand()])
     end
-
-    @testset "minmax_heapify tests" begin
-        for i = 1:20
-            A = rand(50)
-            minmax_heapify!(A)
-            @test is_minmax_heap(A)
+    
+    @testset "make heap tests" begin
+        vs = [10, 4, 6, 1, 16, 2, 20, 17, 13, 5]
+        
+        @testset "make from array tests" begin
+            h = binary_minmax_heap(vs)
+            @test length(h) == 10
+            @test !isempty(h)
+            @test top(h) == min(h) == 1
+            @test max(h) == 20
+            @test is_minmax_heap(h)
+        end
+        
+        @testset "make from random arrays tests" begin
+            for i = 1:20
+                A = rand(50)
+                vtree = _make_binary_minmax_heap(typeof(A), A)
+                @test is_minmax_heap(vtree)
+            end
+        end
+        
+        @testset "push! tests" begin
+            h = binary_minmax_heap(Int)
+            
+            @test length(h) == 0
+            @test isempty(h)
+            
+            push!(h, 1)
+            @test top(h) == 1
+            push!(h, 2)
+            @test is_minmax_heap(h)
+            push!(h, 10)
+            @test is_minmax_heap(h)
+            push!(h, rand(Int))
+            @test is_minmax_heap(h)
+            for i = 1:5
+                A = rand(20)
+                hs = binary_minmax_heap(A)
+                for j = 1:4
+                    push!(hs, rand())
+                    @test is_minmax_heap(hs)
+                end
+            end
         end
     end
     
-    @testset "heapmin / heapmax tests" begin
-        @testset "heapmin tests" begin
-            @test heapmin([1]) == 1
-            @test heapmin([1, 2]) == 1
+    @testset "pop! tests" begin
+        @testset "popmin! tests" begin
+            h = binary_minmax_heap([1])
+            @test popmin!(h) == 1
+            @test length(h) == 0
+            @test isempty(h)
+            h = binary_minmax_heap([1, 3, 2])
+            @test popmin!(h) == 1
+            @test length(h) == 2
+            @test is_minmax_heap(h)
+            @test min(h) == 2
             for i = 1:20
                 A = rand(50)
-                minmax_heapify!(A)
-                @test heapmin(A) == minimum(A)
-            end
-        end
-        @testset "heapmax tests" begin
-            @test heapmax([1]) == 1
-            @test heapmax([1, 2]) == 2
-            @test heapmax([1, 3, 2]) == 3
-            for i = 1:20
-                A = rand(50)
-                minmax_heapify!(A)
-                @test heapmax(A) == maximum(A)
-            end
-        end
-    end
-    
-    @testset "heappop tests" begin
-        @testset "heappop_min tests" begin
-            A = [1]
-            @test heappop_min!(A) == 1
-            @test length(A) == 0
-            A = [1, 3, 2]
-            @test heappop_min!(A) == 1
-            @test length(A) == 2
-            @test is_minmax_heap(A)
-            @test heapmin(A) == minimum(A) == 2
-            for i = 1:20
-                A = rand(50)
-                minmax_heapify!(A)
+                h = binary_minmax_heap(A)
                 minval = minimum(A)
-                @test heappop_min!(A) == minval
-                @test length(A) == 49
-                @test is_minmax_heap(A)
-                @test heapmin(A) == minimum(A)
+                @test popmin!(h) == minval
+                @test length(h) == 49
+                @test is_minmax_heap(h)
             end
         end
-        @testset "heappop_max tests" begin
-            A = [1]
-            @test heappop_max!(A) == 1
-            @test length(A) == 0
-            A = [1, 2, 3]
-            @test heappop_max!(A) == 3
-            @test length(A) == 2
-            @test is_minmax_heap(A)
-            A = [1, 3, 2]
-            @test heappop_max!(A) == 3
-            @test length(A) == 2
-            @test is_minmax_heap(A)
-            @test heapmax(A) == maximum(A) == 2
-            for i = 1:20
-                A = rand(50)
-                minmax_heapify!(A)
-                maxval = maximum(A)
-                @test heappop_max!(A) == maxval
-                @test length(A) == 49
-                @test is_minmax_heap(A)
-                @test heapmax(A) == maximum(A)
-            end
-        end
-    end
-    
-    @testset "mm_heappush! tests" begin
-        A = []
-        mm_heappush!(A, 1)
-        @test A[1] == 1
-        mm_heappush!(A, 2)
-        @test is_minmax_heap(A)
-        mm_heappush!(A, 10)
-        @test is_minmax_heap(A)
-        mm_heappush!(A, rand(Int))
-        @test is_minmax_heap(A)
-        for i = 1:5
-            A = rand(20)
-            minmax_heapify!(A)
-            for j = 1:4
-                mm_heappush!(A, rand(Int))
-                @test is_minmax_heap(A)
-            end
+        @testset "popmax! tests" begin
+            h = binary_minmax_heap([1])
+            @test popmax!(h) == 1
+            @test length(h) == 0
+            @test isempty(h)
+            h = binary_minmax_heap([1, 2, 3])
+            @test popmax!(h) == 3
+            @test length(h) == 2
+            @test is_minmax_heap(h)
+            @test max(h) == 2
+            h = binary_minmax_heap([1, 3, 2])
+            @test popmax!(h) == 3
+            @test length(h) == 2
+            @test is_minmax_heap(h)
+            @test max(h) == 2
         end
     end
 
-    @testset "mm_klargest tests" begin
-        A = rand(Int, 50)
-        sorted_A = sort(A)
-        minmax_heapify!(A)
-        @test mm_ksmallest!(A, 50) == sorted_A
-        A = rand(Int, 50)
-        sorted_A = sort(A)
-        minmax_heapify!(A)
-        @test mm_klargest!(A, 50) == sorted_A[end:-1:1]
+    @testset "empty!" begin
+        @testset "ksmallest tests" begin
+            A = rand(Int, 50)
+            sorted_A = sort(A)
+            h = binary_minmax_heap(A)
+            @test empty!(h) == sorted_A
+            @test isempty(h)
+            @test length(h) == 0
+            
+            h = binary_minmax_heap(A)
+            @test ksmallest!(h, 10) == sorted_A[1:10]
+            @test !isempty(h)
+            @test length(h) == 40
+        end
+        @testset "klargest tests" begin
+            A = rand(Int, 50)
+            sorted_A = sort(A, order=Reverse)
+            h = binary_minmax_heap(A)
+            @test empty!(h, Reverse) == sorted_A
+            @test length(h) == 0
+            @test isempty(h)
+            
+            h = binary_minmax_heap(A)
+            @test klargest!(h, 10) == sorted_A[1:10]
+            @test length(h) == 40
+            @test !isempty(h)
+        end
     end
-
 end
