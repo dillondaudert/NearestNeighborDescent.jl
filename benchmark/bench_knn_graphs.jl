a148eafcbce3bf4e0174ec56c36d4faca2351bd1
Dillon Daudert <dillongdaudert@gmail.com>
2019-11-06 16:37:39 -0500

    Don't run benchmark when k >= np

--- benchmark/bench_knn_graphs.jl
+++ benchmark/bench_knn_graphs.jl
@@ -9,17 +9,21 @@ suite = BenchmarkGroup()
 
 knn_graph_types = [HeapKNNGraph]
 dtypes = [Float32, Float64]
-num_points = [50, 250, 1250, 6250]
-dims = [5, 25, 125, 625]
-num_neighbs = [5, 10, 20, 50, 100]
+num_points = [250, 1250, 6250]
+dims = [25, 125, 625]
+num_neighbs = [10, 20, 50, 100]
 metrics = [SqEuclidean, CosineDist]
 
 for graph in knn_graph_types
     for dtype in dtypes
         for np in num_points
             for d in dims
+                Random.seed!(0)
                 data = [2 .* rand(dtype, d) .- 1 for _ in 1:np] # uniform dist in [-1, 1]
                 for k in num_neighbs
+                    if k ≥ np
+                        continue
+                    end
                     for metric in metrics
                         prepath = [string(graph)]
                         postpath = [string(dtype), "N="*string(np), "D="*string(d), "K="*string(k), string(metric)]
