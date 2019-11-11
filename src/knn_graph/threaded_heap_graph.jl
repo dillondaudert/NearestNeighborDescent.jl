2ba22799472295063ccd677d4ea791d9c31441aa
Dillon Daudert <dillongdaudert@gmail.com>
2019-11-11 13:38:04 -0500

    LockHeap - docstring

--- src/knn_graph/threaded_heap_graph.jl
+++ src/knn_graph/threaded_heap_graph.jl
@@ -1,14 +1,19 @@
 # threaded heap knn graph
 
+"""
+LockHeapKNNGraph - uses locks to synchronize the heaps that store the underlying
+graph edge data. The heaps themselves are *not* thread-safe.
+"""
 struct LockHeapKNNGraph{V<:Integer, K, U<:Real} <: ApproximateKNNGraph{V, K, U}
     _knn_heaps::Vector{BinaryMaxHeap{HeapKNNGraphEdge{V, U}}}
     _heap_locks::Vector{ReentrantLock}
 end
 function LockHeapKNNGraph(data::D, k::Integer, metric::PreMetric) where {D <: AbstractVector}
-    
+
     # assert some invariants
-    # TODO
-    
+    k > 0 || error("LockHeapKNNGraph needs positive `k`")
+    length(data) > k || error("LockHeapKNNGraph needs `length(data) > k`")
+
     np = length(data)
     U = result_type(metric, first(data), first(data))
     HeapType = BinaryMaxHeap{HeapKNNGraphEdge{typeof(k), U}}
@@ -24,5 +29,5 @@ function LockHeapKNNGraph(data::D, k::Integer, metric::PreMetric) where {D <: Ab
             _heappush!(knn_heaps[j], HeapKNNGraphEdge(j, i, rev_dist), k)
         end
     end
-    return LockHeapKNNGraph{typeof(k), k, U}(knn_heaps, heap_locks) 
+    return LockHeapKNNGraph{typeof(k), k, U}(knn_heaps, heap_locks)
 end
