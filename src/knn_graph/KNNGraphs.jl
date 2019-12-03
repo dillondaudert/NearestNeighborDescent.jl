d16f9f3f4db36fb599da0facf1642ef8f5dce402
Dillon Daudert <dillongdaudert@gmail.com>
2019-12-03 13:23:28 -0500

    Move ApproximateKNNGraph to abstract.jl

--- src/knn_graph/KNNGraphs.jl
+++ src/knn_graph/KNNGraphs.jl
@@ -5,19 +5,6 @@ using Distances
 using LightGraphs
 using SparseArrays
 
-# Abstract KNN Graph definitions
-"""
-ApproximateKNNGraph{V, K} subtypes are weighted, directed graphs where each vertex
-has exactly `k` forward edges.
-"""
-abstract type ApproximateKNNGraph{V, K, U <: Real} <: AbstractGraph{V} end
-
-# interface
-Base.eltype(::ApproximateKNNGraph{V, K, U}) where {V, K, U} = V
-# all knn graphs are directed
-LightGraphs.is_directed(::Type{<:ApproximateKNNGraph}) = true
-LightGraphs.is_directed(::ApproximateKNNGraph) = true
-
 include("abstract.jl")
 include("heap_edge.jl")
 include("threaded_heap_graph.jl")
--- src/knn_graph/abstract.jl
+++ src/knn_graph/abstract.jl
@@ -1,8 +1,20 @@
-# fallback method definitions for knn graph types
 
 #######################
 # ApproximateKNNGraph #
 #######################
+
+# Abstract KNN Graph definitions
+"""
+ApproximateKNNGraph{V, K} subtypes are weighted, directed graphs where each vertex
+has exactly `k` forward edges.
+"""
+abstract type ApproximateKNNGraph{V, K, U <: Real} <: AbstractGraph{V} end
+
+# interface
+Base.eltype(::ApproximateKNNGraph{V, K, U}) where {V, K, U} = V
+# all knn graphs are directed
+LightGraphs.is_directed(::Type{<:ApproximateKNNGraph}) = true
+LightGraphs.is_directed(::ApproximateKNNGraph) = true
 
 """
     knn_diameter(g::ApproximateKNNGraph{V}, v::V)
@@ -10,7 +22,7 @@
 Compute the diameter of the ball centered on `v` that covers
 all of `v`s approximate k-nearest neighbors.
 """
-function knn_diameter(g::ApproximateKNNGraph{V}, v::V) where V 
+function knn_diameter(g::ApproximateKNNGraph{V}, v::V) where V
     neighbs = outneighbors(g, v)
     return 2 * maximum(weights(g)[neighbs, v])
 end
